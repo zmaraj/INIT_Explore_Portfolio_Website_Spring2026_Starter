ba87ed0394b916aa79c49992bc1c97b1c57a9bba
zmaraj <zsmaraj99@gmail.com>
2026-02-19 18:20:59 -0500

    Made solution a zip file

--- solution/script.js
+++
@@ -1,7 +0,0 @@
-// Frontend javascript w buttons
-function toggleMenu() {
-  const menu = document.querySelector(".mobile-menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
